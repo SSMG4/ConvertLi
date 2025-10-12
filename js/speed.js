e611c254605dd0b63eba7f76229f3595db83ca8c
SSMG4 <matthew.gallon0912@gmail.com>
2025-10-12 20:55:41 +0200

    Add Speed Convertion Logic

---
+++ js/speed.js
@@ -0,0 +1,13 @@
+const speedMap = {
+  'm/s':1,
+  'km/h':1/3.6,
+  'mph':0.44704,
+  'ft/s':0.3048,
+  'knots':0.514444
+};
+
+function convertSpeed(val, from, to){
+  if(isNaN(val)) return 'Invalid';
+  const inMs = val*speedMap[from];
+  return inMs / speedMap[to];
+}
