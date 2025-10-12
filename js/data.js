d0e4f6a8913c1eeb1a58672f4002fcd898832fcf
SSMG4 <matthew.gallon0912@gmail.com>
2025-10-12 20:50:40 +0200

    Add Data Convertion Logic

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const dataMap = {
+  B:1, KB:1024, MB:1048576, GB:1073741824, TB:1099511627776, PB:1125899906842624
+};
+
+function convertData(val, from, to){
+  if(isNaN(val)) return 'Invalid';
+  const inBytes = val*dataMap[from];
+  return inBytes/dataMap[to];
+}
